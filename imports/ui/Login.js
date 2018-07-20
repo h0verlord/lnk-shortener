0ec13d64185647dbc997d24e9d88711b43e3b3bf
h0verlord <richard.zevel@gmail.com>
2018-07-20 16:27:27 +0200

    Set Up login functionality

--- imports/ui/Login.js
+++ imports/ui/Login.js
@@ -2,12 +2,39 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default class Login extends React.Component {
+  onSubmit(e) {
+    e.preventDefault();
+
+    let email = this.emailInput.value.trim();
+    let password = this.passwordInput.value.trim();
+  }
   render() {
     return (
       <div>
-        <h1>Login to Short Link</h1>
-        login form here
-        <Link to="/signup">Have an account</Link>
+        <h1>Short Lnk</h1>
+
+        {this.state.error ? <p>{this.state.error}</p> : undefined}
+
+        <form onSubmit={this.onSubmit.bind(this)}>
+          <input
+            type="email"
+            name="email"
+            ref={input => {
+              this.emailInput = input;
+            }}
+            placeholder="email@email.com"
+          />
+          <input
+            type="password"
+            name="password"
+            ref={input => {
+              this.passwordInput = input;
+            }}
+            placeholder="Password"
+          />
+          <button>Login</button>
+        </form>
+        <Link to="/signup">Create an account</Link>
       </div>
     );
   }
