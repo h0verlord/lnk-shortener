2e7e3c569d4733600fca4295f09cd1d786164fd9
Unknown <richard.zevel@gmail.com>
2018-07-23 17:24:22 +0200

    Fiddling with SimpleSchema validations

--- server/main.js
+++ server/main.js
@@ -1,5 +1,51 @@
 import { Meteor } from "meteor/meteor";
+import SimpleSchema from "simpl-schema";
 
 Meteor.startup(() => {
   // code to run on server at startup
+
+  //   const petSchema = new SimpleSchema({
+  //       name: {
+  //           type: String,
+  //           min: 1,
+  //           max: 40,
+  //           optional: true,
+  //       },
+  //       age: {
+  //           type: Number,
+  //           min: 0
+  //       },
+  //       contactNumber:{
+  //           type: String,
+  //           optional: true,
+  //           regEx: SimpleSchema.RegEx.Phone
+  //       }
+  //   })
+
+  //   petSchema.validate({
+  //       age: 3,
+  //       contactNumber: 'wfwefw'
+//   //   })
+
+//   const employeeSchema = new SimpleSchema({
+//       name:{
+//           type: String,
+//           min: 1,
+//           max: 200
+//       },
+//       hourlyWage:{
+//           type: Number,
+//           min: 0
+//       },
+//       email: {
+//           type: String,
+//           regEx: SimpleSchema.RegEx.EmailWithTLD
+//       }
+//   });
+
+//   employeeSchema.validate({
+//       name: 'John',
+//       hourlyWage: 0,
+//       email: 'mail.mail.mail'
+//   })
 });
