71a75e4b98fa0b90f5632f063261b3227b8a41b8
h0verlord <richard.zevel@gmail.com>
2018-07-30 16:57:08 +0200

    Old way of displaying DB DATA

--- client/main.js
+++ client/main.js
@@ -1,14 +1,15 @@
 import { Meteor } from "meteor/meteor";
 import React from "react";
 import ReactDOM from "react-dom";
-import {Tracker} from 'meteor/tracker'
+import { Tracker } from "meteor/tracker";
 import { AppRouter, history, onAuthChange } from "../imports/routes/AppRouter";
+import { Links } from "../imports/api/links";
 
 Tracker.autorun(() => {
-    const isAuthenticated = !!Meteor.userId();
-    onAuthChange(isAuthenticated)
-    console.log("isAuthenticated", isAuthenticated);
-  });
+  const isAuthenticated = !!Meteor.userId();
+  onAuthChange(isAuthenticated);
+  console.log("isAuthenticated", isAuthenticated);
+});
 
 Meteor.startup(() => {
   ReactDOM.render(<AppRouter />, document.getElementById("app"));
--- imports/ui/Link.js
+++ imports/ui/Link.js
@@ -1,20 +1,45 @@
 import React from "react";
-import {Accounts} from 'meteor/accounts-base'
-import {history }from './../routes/AppRouter'
+import { Accounts } from "meteor/accounts-base";
+import { history } from "./../routes/AppRouter";
+import { Links } from "../api/links";
+
+import LinksList from './LinksList'
+
 export default class Link extends React.Component {
-    componentWillMount() {
-        if (!Meteor.userId()) {
-            history.replace('/')
-        }
-    };
-    onLogout(){
-        Accounts.logout();
+  componentWillMount() {
+    if (!Meteor.userId()) {
+      history.replace("/");
     }
+  }
+  onLogout() {
+    Accounts.logout();
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    const url = this.urlInput.value.trim();
+    if (url) {
+      Links.insert({ url });
+      this.urlInput.value = "";
+    }
+  }
   render() {
     return (
       <div>
-      <h1>Links Page</h1>
+        <h1>Links Page</h1>
         <button onClick={this.onLogout.bind(this)}>Log Out</button>
+        <LinksList />
+        <p>Add a Link</p>
+        <form onSubmit={this.onSubmit.bind(this)}>
+          <input
+            type="text"
+            ref={url => {
+              this.urlInput = url;
+            }}
+            placeholder="URL"
+          />
+          <button>Add Link</button>
+        </form>
       </div>
     );
   }
