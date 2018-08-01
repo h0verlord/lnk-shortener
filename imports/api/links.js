47a90a1e1d7c1edf01cedd0d29817b6968a72ae9
h0verlord <richard.zevel@gmail.com>
2018-08-01 16:16:54 +0200

    shortID, secure redirects

--- imports/api/links.js
+++ imports/api/links.js
@@ -1,6 +1,7 @@
 import { Meteor } from "meteor/meteor";
 import { Mongo } from "meteor/mongo";
 import SimpleSchema from "simpl-schema";
+import shortid from 'shortid'
 
 export const Links = new Mongo.Collection("links");
 
@@ -23,9 +24,8 @@ Meteor.methods({
       }
     }).validate({ url });
 
-    throw new Meteor.Error(400, e.message);
-
     Links.insert({
+      _id: shortid.generate(),
       url,
       userId: this.userId
     });
