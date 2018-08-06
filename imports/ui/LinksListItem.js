4b00fdac478db19448220f8aa8737a8115c36ab0
h0verlord <richard.zevel@gmail.com>
2018-08-06 12:50:06 +0200

    Link copying to clipboard, conditional rendering

--- imports/ui/LinksListItem.js
+++ imports/ui/LinksListItem.js
@@ -1,20 +1,52 @@
 import React from "react";
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
+import Clipboard from "clipboard";
 
-export default class LinksListItem extends React.Component{
-    render(){
-        return(
-            <div>
-                <h3>{this.props.url}</h3>
-                <h4>{this.props.shortUrl}</h4>
-            </div>
-        )
-    }
+export default class LinksListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      justCopied: false
+    };
+  }
+
+  componentDidMount() {
+    this.clipboard = new Clipboard(this.refCopy);
+    this.clipboard
+      .on("success", () => {
+        this.setState({ justCopied: true });
+        setTimeout(() => {
+          this.setState({ justCopied: false });
+        }, 1000);
+      })
+      .on("error", () => {
+        alert("unable to copy");
+      });
+  }
+  componentWillUnmount() {
+    this.clipboard.destroy();
+  }
+  render() {
+    return (
+      <div>
+        <h3>{this.props.url}</h3>
+        <h4>{this.props.shortUrl}</h4>
+        <button
+          ref={copy => {
+            this.refCopy = copy;
+          }}
+          data-clipboard-text={this.props.shortUrl}
+        >
+          {this.state.justCopied ? "Copied" : "Copy"}
+        </button>
+      </div>
+    );
+  }
 }
 
 LinksListItem.propTypes = {
-    _id: PropTypes.string,
-    shortUrl: PropTypes.string,
-    url: PropTypes.string,
-    userId: PropTypes.string,
-}
+  _id: PropTypes.string,
+  shortUrl: PropTypes.string,
+  url: PropTypes.string,
+  userId: PropTypes.string
+};
