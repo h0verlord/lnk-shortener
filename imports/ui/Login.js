38c325a7cdeed7e60e86a88ff6962a293177da29
h0verlord <richard.zevel@gmail.com>
2018-08-07 13:11:15 +0200

    Login, Signup and NotFound styles basic

--- imports/ui/Login.js
+++ imports/ui/Login.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Meteor } from 'meteor/meteor';
-import {history} from './../routes/AppRouter'
+import React from "react";
+import { Link } from "react-router-dom";
+import { Meteor } from "meteor/meteor";
+import { history } from "./../routes/AppRouter";
 
 export default class Login extends React.Component {
   componentWillMount() {
     if (Meteor.userId()) {
-        history.replace('/links')
+      history.replace("/links");
     }
   }
   constructor(props) {
@@ -23,40 +23,46 @@ export default class Login extends React.Component {
     let password = this.passwordInput.value.trim();
 
     Meteor.loginWithPassword({ email }, password, err => {
-        if (err) {
-            this.setState({error: err.reason})
-        } else {
-            this.setState({error: ''})
-        }
+      if (err) {
+        this.setState({ error: err.reason });
+      } else {
+        this.setState({ error: "" });
+      }
     });
   }
   render() {
     return (
-      <div>
-        <h1>Short Lnk</h1>
+      <div className="boxed-view">
+        <div className="boxed-view__box">
+          <h1>Short Lnk</h1>
 
-        {this.state.error ? <p>{this.state.error}</p> : undefined}
+          {this.state.error ? <p>{this.state.error}</p> : undefined}
 
-        <form onSubmit={this.onSubmit.bind(this)} noValidate>
-          <input
-            type="email"
-            name="email"
-            ref={input => {
-              this.emailInput = input;
-            }}
-            placeholder="email@email.com"
-          />
-          <input
-            type="password"
-            name="password"
-            ref={input => {
-              this.passwordInput = input;
-            }}
-            placeholder="Password"
-          />
-          <button>Login</button>
-        </form>
-        <Link to="/signup">Create an account</Link>
+          <form
+            onSubmit={this.onSubmit.bind(this)}
+            noValidate
+            className="boxed-view__form"
+          >
+            <input
+              type="email"
+              name="email"
+              ref={input => {
+                this.emailInput = input;
+              }}
+              placeholder="email@email.com"
+            />
+            <input
+              type="password"
+              name="password"
+              ref={input => {
+                this.passwordInput = input;
+              }}
+              placeholder="Password"
+            />
+            <button>Login</button>
+          </form>
+          <Link to="/signup">Create an account</Link>
+        </div>
       </div>
     );
   }
--- imports/ui/notFound.js
+++ imports/ui/notFound.js
@@ -1,6 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default () => {
-    return <p>NotFound Component</p>
-}
-
+  return (
+    <div className="boxed-view">
+      <div className="boxed-view__box">
+        <h1>Page Not Found</h1>
+        <p>Huh, I guess you FUCKED UP somewhere...</p>
+        <Link to="/" className="btn">Home</Link>
+      </div>
+    </div>
+  );
+};
